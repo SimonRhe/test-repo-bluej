e3b6eb056aa1e5b60fb9c83f083dfa845a5d5607
SimonRhe <sprheaume@gmail.com>
2018-10-26 15:26:51 +0200

    Initial project setup done with BlueJ. BlueJ-internal files not included or committed

---
+++ test-project/Main.java
@@ -0,0 +1,21 @@
+/**
+ * Test project
+ * @author SPR
+ * @version 2018-10-26
+ */
+
+import java.io.*;
+
+public class Main
+{
+
+    public static void main(String[] args) throws IOException {
+
+            System.out.println("Test project works!");
+
+            
+        
+    }    
+
+}
+    
